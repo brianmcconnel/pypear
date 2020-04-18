cd4f4735e5ba8295796ec863417ce929945ad5d3
Brian McConnel <brianmcconnel@gmail.com>
2020-04-17 21:35:59 -0700

    added build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+BUILD=$(date +"%Y.%m.%d%H%M")
+echo "Building pypear..."
+echo "pypear-$BUILD"
+echo "Submodule init..."
+git submodule update --init
+echo "Installing git-archive-all..."
+pip install git-archive-all
+echo "Making build..."
+mkdir -p build
+echo "Creating build archive..."
+git archive-all build/pypear-$BUILD.tar.gz
