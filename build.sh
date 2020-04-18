3217d7996cb2866058c9019e97194f3351ba55a8
Brian McConnel <brianmcconnel@gmail.com>
2020-04-17 22:37:40 -0700

    updated build script

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ pip install git-archive-all
 echo "Making build..."
 mkdir -p build
 echo "Creating build archive..."
-git archive-all build/pypear-$BUILD.tar.gz
+git-archive-all build/pypear-$BUILD.tar.gz
 echo "Generating hash for conda-forge..."
 openssl sha256 build/pypear-$BUILD.tar.gz
