2e270cca1094f700144207391105aadcd0c89e7d
Brian McConnel <brianmcconnel@gmail.com>
2020-04-19 12:55:23 -0700

    update

--- Makefile
+++ Makefile
@@ -1,18 +1,18 @@
 clean:
-	rm -rf vim-bundles
+	rm -rf pypear/vim-bundles
 	rm -rf pypear.egg-info
 	rm -rf build
 	rm -rf dist
 
 
 build: clean
-	mkdir -p vim-bundles
+	mkdir -p pypear/vim-bundles
 	echo "Installing git-archive-all"
 	pip install git-archive-all
 	echo "Submodule init..."
 	git submodule update --init
 	echo "Creating vim plugin archive..."
-	git archive-all vim-bundles/plugins.zip
+	git archive-all pypear/vim-bundles/plugins.zip
 
 install: build
 	python setup.py install
--- setup.py
+++ setup.py
@@ -3,22 +3,15 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
    requirements = [line.strip() for line in f]
 
-print(requirements)
-
-with open('README.md') as f:
-    long_description = f.readlines()
-
 setup(name='pypear',
       version='0.1.0',
       author='Brian McConnel',
       author_email='brianmcconnel@gmail.com',
       url='https://github.com/brianmcconnel/pypear',
       description='Adequate terminal based python development.',
-      # long_description=long_description,
-      # long_description_content_type ="text/markdown",
       license='MIT',
       packages=find_packages(),
-      package_data={"vim-bundles": ["vim-bundles/*"]},
+      package_data={"pypear": ["vim-bundles/plugins.zip"]},
       entry_points={'console_scripts': ['pypear = pypear.main:main']},
       classifiers=("Programming Language :: Python :: 3",
                    "License :: OSI Approved :: MIT License",
